35c058e6efc17fe85f79fd9caf1c0a9a0983e2a8
Suphakit P <me@suphakit.net>
2023-07-25 01:00:56 +0700

    chore<pages>: clearing template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,43 +11,15 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>
+			&copy; 2023 <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+		</p>
 	</footer>
 </div>
 
 <style>
 	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		height: 100dvh;
 	}
 </style>
