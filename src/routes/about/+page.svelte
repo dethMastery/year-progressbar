2e0fb2060462eccc0cdbe26f644d72f51859cdbc
Suphakit P <me@suphakit.net>
2023-07-25 17:23:36 +0700

    feat<pages>: added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import TW_CLASS from "../../constants/tailwind_class"
+
+
+</script>
+
+<section>
+  <div class="w-full h-screen mx-auto flex flex-col items-center text-center justify-center md:w-[60%] ">
+    <h1 class={TW_CLASS.heading.xxxl}>
+      About
+    </h1>
+
+    <p class="mb-2">
+      This website is made with inspiration from Twitter Account [which is now X's account] name 
+      <a href="https://twitter.com/ProgressBar202_" class={TW_CLASS.link}>Progress Bar 2023</a>
+    </p>
+    <p class="mt-2">
+      Also, This project is a kind of educational project to improving all of my skill in `SvelteKit` and all of my logic works (and try deta.space too lmao)
+    </p>
+  </div>
+</section>
+
+<style>
+
+</style>
